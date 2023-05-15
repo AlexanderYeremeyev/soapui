8035195489c9064e6c38e3f79233597ff70c7459
alexander.yeremeyev <alex-tula@yandex-team.ru>
2023-05-15 19:21:52 +0300

    move listeners to separate classes

--- soapui/src/main/java/com/eviware/soapui/ui/navigator/Navigator.java
+++ soapui/src/main/java/com/eviware/soapui/ui/navigator/Navigator.java
@@ -18,6 +18,7 @@ package com.eviware.soapui.ui.navigator;
 
 import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.model.ModelItem;
+import com.eviware.soapui.model.Releasable;
 import com.eviware.soapui.model.project.Project;
 import com.eviware.soapui.model.tree.SoapUITreeNode;
 import com.eviware.soapui.model.tree.SoapUITreeNodeRenderer;
@@ -77,14 +78,15 @@ import java.util.Set;
  * @author Ole.Matzura
  */
 
-public class Navigator extends JPanel {
+public class Navigator extends JPanel implements Releasable {
     public static final String NAVIGATOR = "navigator";
     private Workspace workspace;
-    private JTree mainTree;
+    private NavigatorTree mainTree;
     private ApiServantFilteredTreeModel treeModel;
     private Set<NavigatorListener> listeners = new HashSet<NavigatorListener>();
     private NavigatorNodesExpandStateEngine navigatorNodesExpandStateEngine;
-
+    private NavigatorTreeMouseListener navigatorTreeMouseListener;
+    private NavigatorTreeKeyListener navigatorTreeKeyListener;
 
     public Navigator(Workspace workspace) {
         super(new BorderLayout());
@@ -126,12 +128,14 @@ public class Navigator extends JPanel {
         mainTree.setExpandsSelectedPaths(true);
         mainTree.setScrollsOnExpand(true);
         mainTree.setToggleClickCount(0);
-        mainTree.addMouseListener(new TreeMouseListener());
+        navigatorTreeMouseListener = new NavigatorTreeMouseListener(mainTree);
+        mainTree.addMouseListener(navigatorTreeMouseListener);
         mainTree.addTreeSelectionListener(new InternalTreeSelectionListener());
         mainTree.setCellRenderer(new SoapUITreeNodeRenderer());
         mainTree.setBorder(null);
         mainTree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
-        mainTree.addKeyListener(new TreeKeyListener());
+        navigatorTreeKeyListener = new NavigatorTreeKeyListener(mainTree);
+        mainTree.addKeyListener(navigatorTreeKeyListener);
         JScrollPane sp = new JScrollPane(mainTree);
         sp.setBorder(BorderFactory.createEmptyBorder());
         List<WindowMargin> extendersList = getNavigatorMarginsList();
@@ -211,55 +215,15 @@ public class Navigator extends JPanel {
         navigatorNodesExpandStateEngine.restoreNodeExpansion();
     }
 
-    private final class TreeKeyListener extends KeyAdapter {
-        public void keyPressed(KeyEvent e) {
-            TreePath selectionPath = mainTree.getSelectionPath();
-            if (selectionPath == null || mainTree.getSelectionCount() == 0) {
-                return;
-            }
-
-            if (mainTree.getSelectionCount() == 1) {
-                SoapUITreeNode lastPathComponent = (SoapUITreeNode) selectionPath.getLastPathComponent();
-                ActionList actions = lastPathComponent.getActions();
-                if (actions != null) {
-                    actions.dispatchKeyEvent(e);
-                }
-
-                if (!e.isConsumed()) {
-                    KeyStroke ks = KeyStroke.getKeyStrokeForEvent(e);
-                    if (ks.equals(UISupport.getKeyStroke("alt C"))) {
-                        mainTree.collapsePath(selectionPath);
-                        e.consume();
-                    } else if (ks.equals(UISupport.getKeyStroke("alt E"))) {
-                        mainTree.collapsePath(selectionPath);
-                        int row = mainTree.getSelectionRows()[0];
-                        TreePath nextPath = mainTree.getPathForRow(row + 1);
-
-                        TreePath path = mainTree.getPathForRow(row);
-                        while (path != null && !path.equals(nextPath)) {
-                            mainTree.expandRow(row);
-                            path = mainTree.getPathForRow(++row);
-                        }
-
-                        e.consume();
-                    }
-                }
-            } else {
-                TreePath[] selectionPaths = mainTree.getSelectionPaths();
-                List<ModelItem> targets = new ArrayList<ModelItem>();
-                for (TreePath treePath : selectionPaths) {
-                    SoapUITreeNode node = (SoapUITreeNode) treePath.getLastPathComponent();
-                    targets.add(node.getModelItem());
-                }
-
-                if (targets.size() > 0) {
-                    ActionList actions = ActionListBuilder
-                            .buildMultiActions(targets.toArray(new ModelItem[targets.size()]));
-                    if (actions.getActionCount() > 0) {
-                        actions.dispatchKeyEvent(e);
-                    }
-                }
-            }
+    @Override
+    public void release() {
+        if (navigatorTreeMouseListener != null) {
+            navigatorTreeMouseListener.release();
+            navigatorTreeMouseListener = null;
+        }
+        if (navigatorTreeKeyListener != null) {
+            navigatorTreeKeyListener.release();
+            navigatorTreeKeyListener = null;
         }
     }
 
@@ -275,184 +239,6 @@ public class Navigator extends JPanel {
                     for (NavigatorListener listener : array) {
                         listener.nodeSelected(newPath == null ? null : treeNode);
 
-                    }
-                }
-            }
-        }
-    }
-
-    public class TreeMouseListener extends MouseAdapter {
-        private final class CollapseRowAction extends AbstractAction {
-            private final int row;
-
-            public CollapseRowAction(int row) {
-                this.row = row;
-            }
-
-            public void actionPerformed(ActionEvent e) {
-                collapseAll(mainTree.getPathForRow(row));
-                mainTree.collapseRow(row);
-            }
-
-            private void collapseAll(TreePath tp) {
-                if (tp == null) {
-                    return;
-                }
-
-                Object node = tp.getLastPathComponent();
-                TreeModel model = mainTree.getModel();
-                if (!model.isLeaf(node)) {
-                    mainTree.collapsePath(tp);
-                    for (int i = 0; i < model.getChildCount(node); i++) {
-                        // for (int i = node.childCount()-4;i>=0;i--){
-                        collapseAll(tp.pathByAddingChild(model.getChild(node, i)));
-                    }
-                    mainTree.collapsePath(tp);
-                }
-            }
-        }
-
-        private final class ExpandRowAction extends AbstractAction {
-            private final int row;
-
-            public ExpandRowAction(int row) {
-                this.row = row;
-            }
-
-            public void actionPerformed(ActionEvent e) {
-                mainTree.expandRow(row);
-                expandAll(mainTree.getPathForRow(row));
-            }
-
-            private void expandAll(TreePath tp) {
-                if (tp == null) {
-                    return;
-                }
-
-                Object node = tp.getLastPathComponent();
-                TreeModel model = mainTree.getModel();
-                if (!model.isLeaf(node)) {
-                    mainTree.expandPath(tp);
-                    for (int i = 0; i < model.getChildCount(node); i++) {
-                        expandAll(tp.pathByAddingChild(model.getChild(node, i)));
-                    }
-                }
-            }
-
-        }
-
-        private ActionList actions;
-
-        private boolean isSingleDesktopMode() {
-            String desktopType = SoapUI.getSoapUICore().getSettings().getString(UISettings.DESKTOP_TYPE, SoapUI.DEFAULT_DESKTOP);
-            return desktopType.equals(SoapUI.SINGLE_DESKTOP);
-        }
-
-        public void mouseClicked(MouseEvent e) {
-            if (e.isPopupTrigger()) {
-                showPopup(e);
-            }
-            if (e.getClickCount() < 2 && !isSingleDesktopMode()) {
-                return;
-            }
-            if (mainTree.getSelectionCount() == 1) {
-                int row = mainTree.getRowForLocation(e.getX(), e.getY());
-                TreePath path = mainTree.getSelectionPath();
-                if (path == null && row == -1) {
-                    return;
-                }
-
-                if (path == null || mainTree.getRowForPath(path) != row) {
-                    mainTree.setSelectionRow(row);
-                }
-
-                SoapUITreeNode node = (SoapUITreeNode) path.getLastPathComponent();
-                actions = node.getActions();
-                if (actions != null) {
-                    SwingUtilities.invokeLater(new Runnable() {
-                        public void run() {
-                            if (actions != null) {
-                                actions.performDefaultAction(new ActionEvent(mainTree, 0, null));
-                                actions = null;
-                            }
-                        }
-                    });
-                }
-            }
-        }
-
-        public void mousePressed(MouseEvent e) {
-            if (e.isPopupTrigger()) {
-                showPopup(e);
-            }
-        }
-
-        public void mouseReleased(MouseEvent e) {
-            if (e.isPopupTrigger()) {
-                showPopup(e);
-            }
-        }
-
-        private void showToolTipLessPopupMenu(JPopupMenu pm, int x, int y) {
-            pm.addPopupMenuListener(new PopupMenuListener() {
-
-                @Override
-                public void popupMenuCanceled(PopupMenuEvent e) {
-                    ToolTipManager.sharedInstance().setEnabled(true);
-                }
-
-                @Override
-                public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-                    ToolTipManager.sharedInstance().setEnabled(true);
-                }
-
-                @Override
-                public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
-                    ToolTipManager.sharedInstance().setEnabled(false);
-                }
-            });
-            pm.show(mainTree, x, y);
-        }
-
-        private void showPopup(MouseEvent e) {
-            if (mainTree.getSelectionCount() < 2) {
-                TreePath path = mainTree.getPathForLocation((int) e.getPoint().getX(), (int) e.getPoint().getY());
-                if (path == null) {
-                    int row = (int) e.getPoint().getY() / mainTree.getRowHeight();
-                    if (row != -1) {
-                        JPopupMenu collapsePopup = new JPopupMenu();
-                        collapsePopup.add("Collapse").addActionListener(new CollapseRowAction(row));
-                        collapsePopup.add("Expand").addActionListener(new ExpandRowAction(row));
-                        showToolTipLessPopupMenu(collapsePopup, e.getX(), e.getY());
-                    }
-
-                    return;
-                }
-                SoapUITreeNode node = (SoapUITreeNode) path.getLastPathComponent();
-
-                JPopupMenu popupMenu = node.getPopup();
-                if (popupMenu == null) {
-                    return;
-                }
-
-                mainTree.setSelectionPath(path);
-
-                showToolTipLessPopupMenu(popupMenu, e.getX(), e.getY());
-            } else {
-                TreePath[] selectionPaths = mainTree.getSelectionPaths();
-                List<ModelItem> targets = new ArrayList<ModelItem>();
-                for (TreePath treePath : selectionPaths) {
-                    SoapUITreeNode node = (SoapUITreeNode) treePath.getLastPathComponent();
-                    targets.add(node.getModelItem());
-                }
-
-                if (targets.size() > 0) {
-                    ActionList actions = ActionListBuilder
-                            .buildMultiActions(targets.toArray(new ModelItem[targets.size()]));
-                    if (actions.getActionCount() > 0) {
-                        JPopupMenu popup = new JPopupMenu();
-                        ActionSupport.addActions(actions, popup);
-                        showToolTipLessPopupMenu(popup, e.getX(), e.getY());
                     }
                 }
             }
